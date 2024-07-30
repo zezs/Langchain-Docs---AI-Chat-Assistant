dad76c268ee41a2bdfc4ea7e6274f02cbca32194
zezs <my.version.ctrl@gmail.com>
2024-07-31 01:31:03 +0200

    session state and memory implementation

--- backend/core.py
+++ backend/core.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from typing import Any
 
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
@@ -8,11 +9,12 @@ from langchain import hub #prompt community
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
 load_dotenv()
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: list[dict[str, Any]] = []):
     print("Retrieving...")
 
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -20,32 +22,25 @@ def run_llm(query: str):
     vectorstore = PineconeVectorStore( index_name=os.environ["INDEX_NAME"], embedding=embeddings)
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-
     combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    retrival_chain = create_retrieval_chain(retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain)
-    # retrival_chain = RetrievalQA.from_chain_type(
-    #     llm=llm,
-    #     chain_type="stuff",
-    #     retriever=vectorstore.as_retriever(),
-    #     prompt=retrieval_qa_chat_prompt
-    # )
 
-    result = retrival_chain.invoke(input={"input": query})
-
-    new_result = {
-        "query": result["input"],
-        "result": result["answer"],
-        "source_documents": result["context"],
-    }
+    #this prompt modifies the original question by combining it with chat history, gving us a better prompt that will help to reriever better result 
+    #43. 5:00
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=llm, retriever=vectorstore.as_retriever(), prompt=rephrase_prompt
+    )
     
-    return new_result
+    retrival_chain = create_retrieval_chain(retriever=history_aware_retriever, combine_docs_chain=combine_docs_chain)
+    # retrival_chain = create_retrieval_chain(retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain)
+    result = retrival_chain.invoke(input={"input": query, "chat_history":chat_history})
+    
+    return result
 
 
 if __name__=="__main__":
     res = run_llm(query="What are agents?")
-    print("\nQUERY: ", res["query"])
-    print("\nRESULT: ", res["result"])
-    print("\nSOURCE: ", res["source_documents"])
+
     
 
     
--- main.py
+++ main.py
@@ -3,15 +3,7 @@ import streamlit as st
 from streamlit_chat import message
 
 
-st.header("Langchain Docs - AI Chat Assistant")
-prompt = st.text_input("Prompt", placeholder="Enter your prompt here...")
-
-# init session state varibles which hold data persistant until the session is over
-if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"] = []
-
-if "chat_answer_history" not in st.session_state:
-    st.session_state["chat_answers_history"] = []
+st.header("Langchain Docs 🦜🔗 - AI Chat Assistant")
 
 # custom func
 def create_sources_string(source_urls: set[str]) -> str:
@@ -24,22 +16,41 @@ def create_sources_string(source_urls: set[str]) -> str:
         sources_string += f"{i+1}. {source}\n"
     return sources_string
 
+# init session state varibles which hold data persistant until the session is over
+# implementing chat history
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
+    st.session_state["chat_answers_history"] = []
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_history"] = []
+
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here...") or st.button("Submit")
 
 if prompt:
     with st.spinner("Generating response..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(query=prompt, chat_history=st.session_state["chat_history"]) #chat_history-> list of tuples, [tuple (first ele->role, second element->content)]
        
         # gathering sources/ set to avoid duplicates
-        sources = set( 
-            [doc.metadata["source"] for doc in generated_response["source_documents"]]
-            )
+        # sources = set( 
+        #     [doc.metadata["source"] for doc in generated_response["context"]]
+        #     )
+        sources = set(doc.metadata["source"] for doc in generated_response["context"])
         
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
+        # storing query and response
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+
+        #storing chat history
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["answer"]))
 
 
 # displaying previosuly entered prompts and generated answers
